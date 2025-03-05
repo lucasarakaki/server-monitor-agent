b1996766342072bdba076b4b9d5c4a24a48d6d33
lucasarakaki <takeshioficial01@gmail.com>
2025-03-05 11:25:58 -0300

    fix: added loading config after loading dotenv

--- bootstrap.php
+++ bootstrap.php
@@ -3,9 +3,9 @@
 // Loading autoloading composer
 require __DIR__.'/vendor/autoload.php';
 
-// Loading config
-$config = require __DIR__.'/config/agent.php';
-
 // Loading dotenv
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
+
+// Loading config
+$config = require __DIR__.'/config/agent.php';
