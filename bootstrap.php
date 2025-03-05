0c2839f7c5ff2920d599dce2efe3868a8be3b923
lucasarakaki <takeshioficial01@gmail.com>
2025-03-04 22:06:40 -0300

    chore: setting up lib dotenv

--- bootstrap.php
+++ bootstrap.php
@@ -1,4 +1,11 @@
 <?php
 
 // Loading autoloading composer
-require __DIR__ . '/vendor/autoload.php';
+require __DIR__.'/vendor/autoload.php';
+
+// Loading config
+$config = require __DIR__.'/config/agent.php';
+
+// Loading dotenv
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
---
+++ config/agent.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'api' => [
+        'url'   => $_ENV['BASE_URL'],
+        'token' => $_ENV['API_KEY'],
+    ],
+];
